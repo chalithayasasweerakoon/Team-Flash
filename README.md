00bf482a09d3e27951159a40da3c5a366f33074d
chalithayasasweerakoon <menukashavindra@yahoo.com>
2020-10-04 21:11:09 +0530

    Team Flash

---
+++ README.md
@@ -0,0 +1,15 @@
+#Android-Mini-Project
+In this project a Simple Daily Expense Tracker is made.
+Expenses are categorized as;
+	*Food
+	*Transport
+	*Shopping expenses 
+
+Technologies used;
+* Android
+* Android in built MySQL Lite Database
+
+Members Involved
+* Chalitha Yasas Weerakoon
+* Chalana Yasas Weerakoon
+* Menuka Shavindra Ranathunga
---
+++ app/src/main/java/com/expensemanager/teamflash/splash.java
@@ -0,0 +1,34 @@
+package com.expensemanager.teamflash;
+//
+//
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+//
+//
+ public  class splash extends AppCompatActivity {
+//
+//    //Button start_btn;
+      ImageButton fab;
+
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+//
+           fab = findViewById(R.id.fab);
+//
+           fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(splash.this, MainActivity.class));
+
+            }
+             });
+//
+//
+     }
+//
+}
